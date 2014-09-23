8a7850c5fe20c9255b6ac0c38ccdba6b78abaeca
briankishinevsky <brian.kishinevsky@yahoo.com>
2014-09-22 20:31:59 -0400

    Got rid of foo()

--- 1/src/brian/Hello.java
+++ 1/src/brian/Hello.java
@@ -6,14 +6,6 @@ public class Hello {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		foo();
 		System.out.println("Hello, Brian!");
 	}
-
-	private static void foo() {
-		int a = 5;
-		int b = 3 + a;
-		System.out.println("b = "+b);
-	}
-
 }
